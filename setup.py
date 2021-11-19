0c7f2e9a88e910fc9d38440d760b692a38efd933
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2021-11-19 23:04:45 +0530

    updated versioning a bit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name="lemon-tools",
-      version="1.0.0",
+      version="1.0.1",
       # if you want a cleaner versioning use the things below
       # setup_requires=['setuptools_scm'],
       # intall_requires=['setuptools_scm'],
